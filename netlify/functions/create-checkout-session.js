18a56bcc06e5cf0d6a8cb7929e56608ff9b5b05a
Fredpelleg <fpelleg@gmail.com>
2024-07-31 22:04:23 +0200

    Update create-checkout-session.js

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_NEW_SECRET_KEY);
 
 exports.handler = async (event) => {
   const { amount, currency, email, reservationId, clientConsent, reservationDuration } = JSON.parse(event.body);
