3451bdee57048c2c34d257b11fb19d80a07f7ffb
Fredpelleg <fpelleg@gmail.com>
2024-07-31 17:49:41 +0200

    Update create-checkout-session.js

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,21 +1,46 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
-  const { amount, currency, email, reservationId, clientConsent } = JSON.parse(event.body);
+  const { amount, currency, email, reservationId, clientConsent, reservationDuration } = JSON.parse(event.body);
 
   try {
+    // Créer la première autorisation
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: currency,
       payment_method_types: ['card'],
       capture_method: 'manual',
-      description: reservationId,  // Utiliser uniquement l'ID de réservation dans la description
+      description: reservationId,
       metadata: {
         email: email,
         client_consent: clientConsent,
-        stay_duration: 'X days',
+        reservation_duration: reservationDuration,
       },
     });
+
+    if (reservationDuration > 7) {
+      // Planifier la réautorisation après 7 jours moins 2 jours avant la fin de la réservation
+      const reauthorizationTime = (reservationDuration - 7) * 24 * 60 * 60 * 1000 - 2 * 24 * 60 * 60 * 1000;
+      
+      setTimeout(async () => {
+        // Annuler l'autorisation existante
+        await stripe.paymentIntents.cancel(paymentIntent.id);
+
+        // Créer une nouvelle autorisation pour le montant total
+        await stripe.paymentIntents.create({
+          amount: amount,
+          currency: currency,
+          payment_method_types: ['card'],
+          capture_method: 'manual',
+          description: reservationId,
+          metadata: {
+            email: email,
+            client_consent: clientConsent,
+            reservation_duration: reservationDuration,
+          },
+        });
+      }, reauthorizationTime);
+    }
 
     return {
       statusCode: 200,
