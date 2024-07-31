6b475680951d2948bf8613b3f490e5a54d18fd40
Fredpelleg <fpelleg@gmail.com>
2024-07-31 21:58:16 +0200

    Update create-checkout-session.js

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -3,8 +3,12 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 exports.handler = async (event) => {
   const { amount, currency, email, reservationId, clientConsent, reservationDuration } = JSON.parse(event.body);
 
-  // Vérifiez que toutes les variables sont définies
+  // Logs de validation des champs
+  console.log('Received data:', { amount, currency, email, reservationId, clientConsent, reservationDuration });
+
+  // Vérifiez que toutes les variables sont définies et non nulles
   if (!amount || !currency || !email || !reservationId || !clientConsent || !reservationDuration) {
+    console.error('All fields are required:', { amount, currency, email, reservationId, clientConsent, reservationDuration });
     return {
       statusCode: 400,
       body: JSON.stringify({ error: 'All fields are required.' }),
@@ -16,7 +20,7 @@ exports.handler = async (event) => {
     const endDate = new Date(now.getTime() + (parseInt(reservationDuration, 10) * 24 * 60 * 60 * 1000) + (2 * 24 * 60 * 60 * 1000));
 
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
+      amount: parseInt(amount, 10), // S'assurer que le montant est un entier
       currency: currency,
       payment_method_types: ['card'],
       capture_method: 'manual', // Pour créer une autorisation
@@ -25,7 +29,7 @@ exports.handler = async (event) => {
       metadata: {
         email: email,
         client_consent: clientConsent,
-        reservation_duration: reservationDuration.toString(), // Assurez-vous que reservationDuration est un nombre ou une chaîne valide
+        reservation_duration: reservationDuration.toString(),
         end_date: endDate.toISOString(),
         is_caution: 'true',
       },
