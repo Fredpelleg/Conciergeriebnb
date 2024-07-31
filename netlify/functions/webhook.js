d010e635dfc1f9b2e24952a41a0ab154c04e5953
Fredpelleg <fpelleg@gmail.com>
2024-07-31 19:12:59 +0200

    Update webhook.js

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -2,43 +2,59 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
   const sig = event.headers['stripe-signature'];
-  let webhookEvent;
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+  let stripeEvent;
 
   try {
-    webhookEvent = stripe.webhooks.constructEvent(event.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    stripeEvent = stripe.webhooks.constructEvent(event.body, sig, endpointSecret);
   } catch (err) {
-    return { statusCode: 400, body: `Webhook Error: ${err.message}` };
+    return {
+      statusCode: 400,
+      body: `Webhook Error: ${err.message}`,
+    };
   }
 
-  switch (webhookEvent.type) {
-    case 'payment_intent.requires_action':
-      const paymentIntent = webhookEvent.data.object;
-      const createdDate = new Date(paymentIntent.created * 1000);
-      const currentDate = new Date();
-      const diffDays = Math.floor((currentDate - createdDate) / (1000 * 60 * 60 * 24));
+  switch (stripeEvent.type) {
+    case 'payment_intent.canceled':
+      const paymentIntent = stripeEvent.data.object;
+      const reservationDuration = parseInt(paymentIntent.metadata.reservation_duration);
+      const endDate = new Date(paymentIntent.metadata.end_date);
+      const now = new Date();
 
-      if (diffDays >= 6) {
+      if (reservationDuration > 7 && now < endDate) {
+        const newReservationDuration = reservationDuration - 7;
+        const newEndDate = new Date(now.getTime() + (newReservationDuration * 24 * 60 * 60 * 1000) + (2 * 24 * 60 * 60 * 1000));
+
         try {
-          // Annuler l'autorisation existante
-          await stripe.paymentIntents.cancel(paymentIntent.id);
-
-          // Créer une nouvelle autorisation avec les mêmes métadonnées
-          const newPaymentIntent = await stripe.paymentIntents.create({
+          await stripe.paymentIntents.create({
             amount: paymentIntent.amount,
             currency: paymentIntent.currency,
-            payment_method: paymentIntent.payment_method,
+            payment_method_types: ['card'],
             capture_method: 'manual',
-            metadata: paymentIntent.metadata,
+            description: paymentIntent.description,
+            metadata: {
+              email: paymentIntent.metadata.email,
+              client_consent: paymentIntent.metadata.client_consent,
+              reservation_duration: newReservationDuration.toString(),
+              end_date: newEndDate.toISOString(), // Mise à jour de la date de fin
+            },
           });
-
-          return { statusCode: 200, body: JSON.stringify({ new_payment_intent: newPaymentIntent }) };
         } catch (error) {
-          return { statusCode: 500, body: `Error reauthorizing payment: ${error.message}` };
+          return {
+            statusCode: 400,
+            body: `Error reauthorizing PaymentIntent: ${error.message}`,
+          };
         }
-      } else {
-        return { statusCode: 200, body: JSON.stringify({ received: true }) };
       }
+      break;
+
     default:
-      return { statusCode: 400, body: 'Unhandled event type' };
+      console.log(`Unhandled event type ${stripeEvent.type}`);
   }
+
+  return {
+    statusCode: 200,
+    body: 'Success',
+  };
 };
