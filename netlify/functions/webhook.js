76c828a2899eca3d3e6587ea4e97ccfaed386844
Fredpelleg <fpelleg@gmail.com>
2024-07-31 16:58:54 +0200

    Update webhook.js

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -1,30 +1,44 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
-  const { amount, currency, email, reservationId, clientConsent } = JSON.parse(event.body);
+  const sig = event.headers['stripe-signature'];
+  let webhookEvent;
 
   try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: currency,
-      payment_method_types: ['card'],
-      capture_method: 'manual',
-      metadata: {
-        email: email,
-        reservation_id: reservationId,
-        client_consent: clientConsent,
-        stay_duration: 'X days',
-      },
-    });
+    webhookEvent = stripe.webhooks.constructEvent(event.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+  } catch (err) {
+    return { statusCode: 400, body: `Webhook Error: ${err.message}` };
+  }
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ clientSecret: paymentIntent.client_secret, id: paymentIntent.id }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: error.message }),
-    };
+  switch (webhookEvent.type) {
+    case 'payment_intent.requires_action':
+      const paymentIntent = webhookEvent.data.object;
+      const createdDate = new Date(paymentIntent.created * 1000);
+      const currentDate = new Date();
+      const diffDays = Math.floor((currentDate - createdDate) / (1000 * 60 * 60 * 24));
+
+      if (diffDays >= 6) {
+        try {
+          // Annuler l'autorisation existante
+          await stripe.paymentIntents.cancel(paymentIntent.id);
+
+          // Créer une nouvelle autorisation avec les mêmes métadonnées
+          const newPaymentIntent = await stripe.paymentIntents.create({
+            amount: paymentIntent.amount,
+            currency: paymentIntent.currency,
+            payment_method: paymentIntent.payment_method,
+            capture_method: 'manual',
+            metadata: paymentIntent.metadata,
+          });
+
+          return { statusCode: 200, body: JSON.stringify({ new_payment_intent: newPaymentIntent }) };
+        } catch (error) {
+          return { statusCode: 500, body: `Error reauthorizing payment: ${error.message}` };
+        }
+      } else {
+        return { statusCode: 200, body: JSON.stringify({ received: true }) };
+      }
+    default:
+      return { statusCode: 400, body: 'Unhandled event type' };
   }
 };
