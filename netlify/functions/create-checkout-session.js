571a060a3a11fb0263511b5295bda8ea41af1732
Fredpelleg <fpelleg@gmail.com>
2024-07-31 19:09:14 +0200

    Update create-checkout-session.js

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -4,43 +4,23 @@ exports.handler = async (event) => {
   const { amount, currency, email, reservationId, clientConsent, reservationDuration } = JSON.parse(event.body);
 
   try {
-    // Créer la première autorisation
+    const now = new Date();
+    const endDate = new Date(now.getTime() + (reservationDuration * 24 * 60 * 60 * 1000) + (2 * 24 * 60 * 60 * 1000));
+
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: currency,
       payment_method_types: ['card'],
       capture_method: 'manual',
       description: reservationId,
+      receipt_email: email, // Ajout du champ receipt_email
       metadata: {
         email: email,
         client_consent: clientConsent,
-        reservation_duration: reservationDuration,
+        reservation_duration: reservationDuration.toString(),
+        end_date: endDate.toISOString(), // Ajout de la date de fin
       },
     });
-
-    if (reservationDuration > 7) {
-      // Planifier la réautorisation après 7 jours moins 2 jours avant la fin de la réservation
-      const reauthorizationTime = (reservationDuration - 7) * 24 * 60 * 60 * 1000 - 2 * 24 * 60 * 60 * 1000;
-      
-      setTimeout(async () => {
-        // Annuler l'autorisation existante
-        await stripe.paymentIntents.cancel(paymentIntent.id);
-
-        // Créer une nouvelle autorisation pour le montant total
-        await stripe.paymentIntents.create({
-          amount: amount,
-          currency: currency,
-          payment_method_types: ['card'],
-          capture_method: 'manual',
-          description: reservationId,
-          metadata: {
-            email: email,
-            client_consent: clientConsent,
-            reservation_duration: reservationDuration,
-          },
-        });
-      }, reauthorizationTime);
-    }
 
     return {
       statusCode: 200,
