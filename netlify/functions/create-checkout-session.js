bff4162ed39cb67a4f94cf0e877627ebf6bb04f7
Fredpelleg <fpelleg@gmail.com>
2024-07-31 15:22:37 +0200

    Update create-checkout-session.js

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,7 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
-  console.log('Using Stripe Secret Key:', process.env.STRIPE_SECRET_KEY); // Vérifiez la clé ici
+  console.log('Using Stripe Secret Key:', process.env.STRIPE_SECRET_KEY);
 
   const { amount, currency, email } = JSON.parse(event.body);
 
@@ -10,7 +10,7 @@ exports.handler = async (event) => {
       amount: amount,
       currency: currency,
       payment_method_types: ['card'],
-      capture_method: 'manual', // Pour créer une autorisation
+      capture_method: 'manual',
       metadata: { email: email }, // Ajout de l'email dans les métadonnées
     });
 
@@ -21,7 +21,7 @@ exports.handler = async (event) => {
       body: JSON.stringify({ clientSecret: paymentIntent.client_secret, id: paymentIntent.id }),
     };
   } catch (error) {
-    console.error('Error creating PaymentIntent:', error.message); // Journaliser l'erreur
+    console.error('Error creating PaymentIntent:', error.message);
     return {
       statusCode: 400,
       body: JSON.stringify({ error: error.message }),
