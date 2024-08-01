8374cb2180fa7fd0c40ae82d8436267f7b46e034
Fredpelleg <fpelleg@gmail.com>
2024-08-01 15:41:39 +0200

    Update create-checkout-session.js

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,46 +1,35 @@
-const stripe = require('stripe')(process.env.STRIPE_NEW_SECRET_KEY);
+// Exemple de fonction Netlify pour créer un PaymentIntent
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
   const { amount, currency, email, reservationId, clientConsent, reservationDuration } = JSON.parse(event.body);
 
-  console.log('Données reçues:', { amount, currency, email, reservationId, clientConsent, reservationDuration });
-
-  if (!amount || !currency || !email || !reservationId || !clientConsent || !reservationDuration) {
-    console.error('Tous les champs sont requis:', { amount, currency, email, reservationId, clientConsent, reservationDuration });
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: 'Tous les champs sont requis.' }),
-    };
-  }
-
   try {
     const now = new Date();
-    const endDate = new Date(now.getTime() + (parseInt(reservationDuration, 10) * 24 * 60 * 60 * 1000) + (2 * 24 * 60 * 60 * 1000));
+    const endDate = new Date(now.getTime() + (reservationDuration * 24 * 60 * 60 * 1000) + (2 * 24 * 60 * 60 * 1000));
 
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: parseInt(amount, 10),
+      amount: amount,
       currency: currency,
       payment_method_types: ['card'],
-      capture_method: 'manual', // Pour créer une autorisation
+      capture_method: 'manual',
       description: reservationId,
-      receipt_email: email,
+      receipt_email: email, // Ajout du champ email
       metadata: {
         email: email,
         client_consent: clientConsent,
         reservation_duration: reservationDuration.toString(),
-        end_date: endDate.toISOString(),
-        is_caution: 'true',
+        end_date: endDate.toISOString(), // Ajout de la date de fin
+        is_caution: true,
       },
     });
-
-    console.log('PaymentIntent créé avec succès:', paymentIntent);
 
     return {
       statusCode: 200,
       body: JSON.stringify({ clientSecret: paymentIntent.client_secret, id: paymentIntent.id }),
     };
   } catch (error) {
-    console.error('Erreur lors de la création du PaymentIntent:', error.message);
+    console.error('Erreur lors de la création de PaymentIntent:', error);
     return {
       statusCode: 400,
       body: JSON.stringify({ error: error.message }),
