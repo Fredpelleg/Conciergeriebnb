76d322948f925f050a4d303d527300b13d79d827
Fredpelleg <fpelleg@gmail.com>
2024-08-01 18:24:09 +0200

    Update webhook.js

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_NEW_SECRET_KEY);
 
 exports.handler = async (event) => {
   const sig = event.headers['stripe-signature'];
