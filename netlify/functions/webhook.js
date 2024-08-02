47a29ba47beb11a8b3e4ad6ec1c44df754b538b3
Fredpelleg <fpelleg@gmail.com>
2024-08-02 06:06:32 +0200

    Update webhook.js

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -1,5 +1,5 @@
-//const stripe = require('stripe')(process.env.STRIPE_NEW_SECRET_KEY);
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_NEW_SECRET_KEY);
+//const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
   const stripeEvent = JSON.parse(event.body);
