fdf33d07568148945a4422f7c46470c9e6c21427
Fredpelleg <fpelleg@gmail.com>
2024-08-02 13:30:34 +0200

    Update webhook.js

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -8,7 +8,7 @@ exports.handler = async (event) => {
   try {
     stripeEvent = stripe.webhooks.constructEvent(event.body, signature, endpointSecret);
   } catch (err) {
-    console.error('Signature error:', err.message);
+    console.error('Erreur de signature:', err.message);
     return { statusCode: 400, body: `Webhook Error: ${err.message}` };
   }
 
@@ -17,39 +17,47 @@ exports.handler = async (event) => {
     console.info(`Caution annulée détectée pour : ${paymentIntent.id}`);
 
     try {
-      // Check if it's a caution to be renewed
-      if (paymentIntent.metadata.is_caution === 'true' && paymentIntent.metadata.reservationDuration > 7) {
+      if (paymentIntent.metadata.is_caution === 'true' && paymentIntent.metadata.reservation_duration > 7) {
         const customerId = paymentIntent.customer;
+        const paymentMethodId = paymentIntent.payment_method;
 
-        if (!customerId) {
-          console.error('Client not attached to payment method:', paymentIntent.payment_method);
-          return { statusCode: 400, body: 'Client not found for the canceled payment intent' };
+        if (!customerId || !paymentMethodId) {
+          console.error('Client ou méthode de paiement non attachée:', paymentMethodId);
+          return { statusCode: 400, body: 'Client ou méthode de paiement non trouvée pour l\'intention de paiement annulée' };
         }
 
-        // Create a new payment intent with the same payment method
+        // Attacher la méthode de paiement si elle n'est pas encore attachée
+        const paymentMethod = await stripe.paymentMethods.retrieve(paymentMethodId);
+
+        if (paymentMethod.customer !== customerId) {
+          await stripe.paymentMethods.attach(paymentMethodId, {
+            customer: customerId,
+          });
+        }
+
         const newPaymentIntent = await stripe.paymentIntents.create({
           amount: paymentIntent.amount,
           currency: paymentIntent.currency,
-          customer: customerId, // Use existing customer
-          payment_method_types: ['card'],
+          customer: customerId,
+          payment_method: paymentMethodId,
           capture_method: 'manual',
-          payment_method: paymentIntent.payment_method, // Reuse the same payment method
+          payment_method_types: ['card'],
           metadata: {
             email: paymentIntent.metadata.email,
-            clientConsent: paymentIntent.metadata.clientConsent,
-            reservationDuration: paymentIntent.metadata.reservationDuration,
+            client_consent: paymentIntent.metadata.client_consent,
+            reservation_duration: paymentIntent.metadata.reservation_duration,
             end_date: paymentIntent.metadata.end_date,
             is_caution: "true"
           },
         });
 
-        console.info(`New payment intent created: ${newPaymentIntent.id}`);
+        console.info(`Nouvelle intention de paiement créée: ${newPaymentIntent.id}`);
       }
     } catch (error) {
-      console.error('Error processing webhook event:', error.message);
-      return { statusCode: 500, body: `Error processing webhook event: ${error.message}` };
+      console.error('Erreur lors du traitement de l\'événement webhook:', error.message);
+      return { statusCode: 500, body: `Erreur lors du traitement de l'événement webhook: ${error.message}` };
     }
   }
 
-  return { statusCode: 200, body: 'Event received' };
+  return { statusCode: 200, body: 'Événement reçu' };
 };
