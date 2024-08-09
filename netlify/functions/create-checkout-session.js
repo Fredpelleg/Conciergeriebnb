2bb6766dd7b65d1ec5a916adc4821020a2a602d3
Fredpelleg <fpelleg@gmail.com>
2024-08-09 15:02:43 +0200

    Update create-checkout-session.js

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,46 +1,29 @@
 const stripe = require('stripe')(process.env.STRIPE_NEW_SECRET_KEY);
-require('dotenv').config();
 
 exports.handler = async (event) => {
   try {
-    const { amount, currency, email, reservationId, clientConsent, reservationDuration, paymentMethodId } = JSON.parse(event.body);
+    const { amount, currency, email, reservationId, clientConsent, reservationDuration } = JSON.parse(event.body);
 
-    // Vérifier si le client existe déjà avec l'e-mail fourni
+    // Recherche du client existant par e-mail
     const customers = await stripe.customers.list({ email, limit: 1 });
     let customerId;
 
     if (customers.data.length > 0) {
       customerId = customers.data[0].id;
-      console.log(`Client existant trouvé: ${customerId}`);
     } else {
-      // Créer un nouveau client si non existant
+      // Créer un nouveau client s'il n'existe pas
       const customer = await stripe.customers.create({
         email,
-        metadata: {
-          clientConsent,
-          reservationId
-        }
+        metadata: { clientConsent, reservationId }
       });
       customerId = customer.id;
-      console.log(`Nouveau client créé: ${customerId}`);
-    }
-
-    // Attacher la méthode de paiement au client s'il n'est pas déjà attaché
-    if (paymentMethodId) {
-      const paymentMethod = await stripe.paymentMethods.retrieve(paymentMethodId);
-      
-      if (paymentMethod.customer !== customerId) {
-        await stripe.paymentMethods.attach(paymentMethodId, { customer: customerId });
-        console.log(`Méthode de paiement ${paymentMethodId} attachée au client ${customerId}`);
-      }
     }
 
     // Créer l'intention de paiement
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency,
-      customer: customerId, // Associer le client à l'intention de paiement
-      payment_method: paymentMethodId,
+      customer: customerId,
       payment_method_types: ['card'],
       capture_method: 'manual',
       metadata: {
@@ -53,14 +36,11 @@ exports.handler = async (event) => {
       }
     });
 
-    console.log(`Intention de paiement créée: ${paymentIntent.id}`);
-
     return {
       statusCode: 200,
       body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
     };
   } catch (error) {
-    console.error('Erreur lors de la création de l\'intention de paiement:', error.message);
     return {
       statusCode: 400,
       body: JSON.stringify({ error: error.message }),
